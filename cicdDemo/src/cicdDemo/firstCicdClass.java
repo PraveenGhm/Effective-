3511c8ea040a3ae195270d30a42f745227717a92
Praveen Ghimire <praveen.ghimire@cu.edu>
2019-11-05 23:38:00 -0700

    initial commit

---
+++ cicdDemo/src/cicdDemo/firstCicdClass.java
@@ -0,0 +1,13 @@
+package cicdDemo;
+
+public class firstCicdClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is the example for cicd pipeline build");
+		
+		
+	}
+
+}
